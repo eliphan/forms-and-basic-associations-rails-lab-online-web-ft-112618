32c8c3c59c7398eeeeee53fb395cccea42255557
Elise Phan <aliceconnic@gmail.com>
2019-02-12 23:50:57 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ActiveRecord::Base
   has_many :notes
   
    def artist_name=(name)
-     self.artist = Category.find_or_create_by(name: name)
+     self.artist = Artist.find_or_create_by(name: name)
    end
  
    def artist_name
