526c7ec3e7fea38f50611772f99b0973a0e3bce3
Elise Phan <aliceconnic@gmail.com>
2019-02-12 23:50:39 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,13 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
   
-  
+   def artist_name=(name)
+     self.artist = Category.find_or_create_by(name: name)
+   end
+ 
+   def artist_name
+      self.artist ? self.artist.name : nil
+   end
   
   
 end
