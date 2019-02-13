4210d5251292642e51c0f4a18871963390e40ba9
Elise Phan <aliceconnic@gmail.com>
2019-02-13 00:30:34 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,7 +48,7 @@ class SongsController < ApplicationController
 
   def song_params
     params.require(:song).permit(:title, :artist_name, :genre_id,:notes_ids => [])
-    
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])    
   end
 end
 
